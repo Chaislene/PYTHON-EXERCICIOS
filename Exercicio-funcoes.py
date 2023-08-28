b154c119c6f72af5c768358a99709bc506ca1f52
Chaislene <129233358+Chaislene@users.noreply.github.com>
2023-08-27 23:54:26 -0300

    Add files via upload

---
+++ Exercicio-funcoes.py
@@ -0,0 +1,22 @@
+# Exercício 01 - Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+"""def soma3(a, b, c):
+    s = a + b + c
+    return s
+
+
+print('SOMA DE TRÊS NÚMEROS')
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+c = int(input('Terceiro número: '))
+
+print('Soma = ', soma3(a, b, c))"""
+
+
+# Exercício 02 - "Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127->721. 
+"""def reverso(n):
+    return str(n[::-1])
+
+n = str(input('Digite um número: ')).strip()
+print(f'Reverso: {reverso(n)}')"""
+
+# Exercício 03 - Escreva um script que pergunta ao usuário se ele deseja converter uma temperatura de grau Celsius para Fahrenheit ou vice-versa. Para cada opção, crie uma função. Crie uma terceira, que é um menu para o usuário escolher a opção desejada, onde esse menu chama a função de conversão correta. 
