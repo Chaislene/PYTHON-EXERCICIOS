658eb583cb4d4b5f0c45679189c61f6a3c9e2aab
Chaislene <129233358+Chaislene@users.noreply.github.com>
2023-08-27 23:54:51 -0300

    Add files via upload

---
+++ Exercicio-extra-de-python.py
@@ -0,0 +1,17 @@
+# Exercício 01 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira, e calcule quanto poderia comprar de cada moeda estrangeira. Considere a tabela de conversão abaixo: 
+# Dólar Americano: R$4,91 
+# Peso Argentino: R$0,02
+# Dólar Australiano: R$3,18
+# Dólar Canadense: R$3,64
+# Franco Suiço: R$0,42
+# Euro: R$5,36 
+# Libra esterlina: R$6,212
+"""n = float(input("Quanto dinheiro você tem? \nR$"))
+dolar_americano = 4.91
+conversao = n / dolar_americano
+print("Com R${} você pode comprar US$ {:.2f}.".format(n, conversao))"""
+
+# Exercício 02 - Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$80,00 por dia e R$0,20 por km rodado.
+
+
+# Exercício 03 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário. Se o salário for até R$1000,00 o funcionário terá 20% de aumento. Entre R$1001,00 até R$2800,00 o funcionário terá 10% de aumento. Acima de R$2801,00 o funcionário terá 5% de aumento.
