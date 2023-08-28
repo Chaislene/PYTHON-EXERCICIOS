525ce3846322ff0365571e6202f1d5aacb0a4a55
Chaislene <129233358+Chaislene@users.noreply.github.com>
2023-08-27 23:53:04 -0300

    Add files via upload

---
+++ Exercicio-conceitos-basicos-de-Python.py
@@ -0,0 +1,31 @@
+# Exercício 01 - Faça um Programa que peça um número e então mostre a mensagem: ->O número informado foi[número].
+"""print('Olá mundo!')
+print('Este é o meu primeiro programa em Python!')
+
+numero = int(input('Digite um numero: '))
+print(f'O número informado foi: {numero}')"""
+
+
+
+# Exercício 02 - Façaum Programa que peça dois números e imprima a soma.
+"""print('Olá mundo!')
+print('Este é o meu primeiro programa em Python!')
+
+numero1 = int(input('Digite o numero 01: '))
+numero2 = int(input('Digite o numero 02: '))
+soma = numero1+numero2
+print(soma)"""
+
+# Exercício 03 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+"""tc = float(input('Temperatura em Celsius: '))
+print ('Temperatura em Fahrenheit: %.2f' %(9*tc/5 + 32))"""
+
+# Exercício 04 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+"""print('Olá mundo!')
+print('Este é o meu primeiro programa em Python!')
+
+numero1 = float(input('Quanto você ganha por hora trabalhada? '))
+numero2 = int(input('Quantas horas você trabalha no mês? '))
+soma = numero1+numero2
+print(soma)"""
+
